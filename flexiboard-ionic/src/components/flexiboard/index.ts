b2639daf571eb91d1786351755ade9cd5bd16d93
Bryce Marshall <dev.brycemarshall@gmail.com>
2017-12-05 14:40:26 +1300

    Include missing export.

--- flexiboard-ionic/src/components/flexiboard/index.ts
+++ flexiboard-ionic/src/components/flexiboard/index.ts
@@ -1,4 +1,4 @@
 export { FlexiboardModule } from './flexiboard.module';
 export { FlexiboardComponent, FlexCompBase, HostCompBase, FlexiBlankComponent, FlexiboardService, FlexiColumnComponent, FlexiContainerComponent, FlexiKeyComponent, FlexiRowComponent } from './flexiboard';
-export { FlexiGroup, FlexiKey, FlexiItem, FlexiBlank, FlexiItemBase } from './interfaces';
+export { FlexiGroup, FlexiKey, FlexiItem, FlexiBlank, FlexiItemBase, KeyInstance } from './interfaces';
 export { FlexiboardDefinitionProvider } from './flexiboard-definition-provider';
